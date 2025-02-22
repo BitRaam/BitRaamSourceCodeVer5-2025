cd4a6710d55a0b53224a4a629531131165e2d6b5
Gabriel Mendoza <gabrielemf@hotmail.com>
2025-02-21 18:03:41 -0800

    update block subsidy

--- src/validation.cpp
+++ src/validation.cpp
@@ -1944,7 +1944,10 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     if (halvings >= 64)
         return 0;
 
-    CAmount nSubsidy = 50 * COIN;
+    if(nHeight == 1)
+        return 10500000 * * COIN;  // goes to MIU and the TM organization
+    
+    CAmount nSubsidy = 25 * COIN;
     // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
     return nSubsidy;
